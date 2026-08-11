f2b5ead0aa3f710d0234da360b5624b89c031d8d
Emmanuel N'dia <emmanuellevi.ndia@gmail.com>
2026-08-11 12:58:26 +0000

    corrections typescript dist/server.js

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,6 +1,6 @@
 import { buildApp } from "./app.js";
 
-const host = process.env.API_HOST ?? "127.0.0.1";
+const host = process.env.API_HOST ?? process.env.HOST ?? "127.0.0.1";
 const port = Number(process.env.API_PORT ?? process.env.PORT ?? 3200);
 const app = buildApp();
 
